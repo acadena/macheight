5b6cfaaa515751c27b0ec2286cc004da4e8ab55e
Andres Cadena <acadena@dayscript.com>
2023-01-18 20:06:28 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,36 @@
+import math
+
 values = input("Please input values: ").split( " " )
 array = values[0].split( "," )
 sum = int(values[1])
 
-index = 1
 success = []
-array_len = len(array)
+exist = []
+array_len = len(array)-1
+mid_array = math.floor(array_len/2)
+
 for value in array:
-    i = index
-    while i < array_len:
-        if( int(value) + int(array[i]) == sum ):
-            success.append( value + " " + array[i] ) 
+   
+    i = 1
+    k = array_len
+
+    while i <= (mid_array) and k >= 0:
+
+        if( int(value) + int(array[i]) == sum and
+            not array[i] in exist and
+            int(value) != int(array[i])
+        ):
+            success.append( value + " " + array[i] )
+            exist.append(value)
+
+        if( int(value) + int(array[k]) == sum and
+            not array[k] in exist and
+            int(value) != int(array[k])
+        ):
+            success.append( value + " " + array[k] )
+            exist.append(value)
+
         i += 1
-    index += 1
+        k -= 1
 
 print( success )
